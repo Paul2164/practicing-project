63282414fe11d5aacab54752b1568a40e56a10c5
Paul <fpaul847@gmail.com>
2022-11-19 16:11:43 +0100

    third commit

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-function dogs() {
+function getDogs() {
 
     return new Promise(function(resolve,reject) {
 
@@ -14,7 +14,74 @@ function dogs() {
     })
 }
 
-dogs().then(function (values) {
+function getCats() {
 
-    console.log(values)
-})
+    return new Promise(function(resolve,reject) {
+
+        const random = Math.random() * 2000;
+
+        setTimeout(function() {
+
+            resolve([
+
+                "Meow", "Plurr", "Black-cat", "White-cat"
+            ])
+        }, random)
+    })
+}
+
+function getBirds() {
+
+    return new Promise(function(resolve,reject) {
+
+        const random = Math.random() * 2000;
+
+        setTimeout(function() {
+
+            resolve([
+
+                "Pigeon", "Dove", "Parrot", "Green-bird"
+            ])
+        }, random)
+    })
+}
+
+// getDogs().then(function (values) {
+
+//     console.log(values)
+
+//     return getCats()
+// }).then (function(value) {
+//     console.log(value)
+
+//     return getBirds()
+
+// }).then(function(value) {
+
+//     console.log(value)
+// })
+
+// let p =  Promise.all([
+
+//     getDogs(),getCats(),getBirds()
+// ])
+
+// p.then(function(results){
+
+//     console.log(results)
+// })
+
+
+async function go() {
+
+
+    const[dogs,cats,birds] = await Promise.all([getDogs(),getCats(),getBirds()])
+
+    const allAnimals = dogs.concat(cats,birds)
+
+    console.log(allAnimals)
+
+
+}
+
+go()
