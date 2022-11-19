e517bbffa3f343cf47901ab37c0168af60c1a82a
Paul <fpaul847@gmail.com>
2022-11-19 17:16:13 +0100

    feature commit 2

--- src/feature.js
+++ src/feature.js
@@ -21,3 +21,26 @@ function addNumbers (num1,num2) {
 
 let numbers = addNumbers(3,5)
 console.log(numbers)
+
+
+let promise4 = Promise.resolve("successful")
+
+let promise5 = Promise.reject("oooops")
+
+let promise6 = new Promise (function(resolve,reject){
+
+    setTimeout(function() {
+
+        resolve("Login successful")
+    })
+})
+
+let promise7 = Promise.allSettled([promise4,promise5,promise6])
+
+promise7.then(function(result) {
+
+    result.forEach(function(value) {
+
+        console.log(value)
+    })
+})
